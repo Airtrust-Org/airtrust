93867a2b536b76c1d361e04779fc39e01d1065eb
Filipe Daumas <airtrust-system@gmail.com>
2026-07-13 00:48:31 -0300

    fix: corrigir codigos SK76 na migration 0426

--- worker-airtrust/migrations/0426_sk76_periodico_nomenclatura.sql
+++ worker-airtrust/migrations/0426_sk76_periodico_nomenclatura.sql
@@ -16,29 +16,29 @@
 UPDATE modelos_sessao
 SET nome = 'SK76 - PERIÓDICO - 01/04 - CICLO 1: VFR',
     updated_at = datetime('now')
-WHERE codigo = 'SK76-C1-VFR' AND deleted_at IS NULL;
+WHERE codigo = 'S76-P-C1/VFR' AND deleted_at IS NULL;
 
 UPDATE modelos_sessao
 SET nome = 'SK76 - PERIÓDICO - 02/04 - CICLO 1: IFR',
     updated_at = datetime('now')
-WHERE codigo = 'SK76-C1-IFR' AND deleted_at IS NULL;
+WHERE codigo = 'S76-P-C1/IFR' AND deleted_at IS NULL;
 
 UPDATE modelos_sessao
 SET nome = 'SK76 - PERIÓDICO - 01/04 - CICLO 2: VFR',
     updated_at = datetime('now')
-WHERE codigo = 'SK76-C2-VFR' AND deleted_at IS NULL;
+WHERE codigo = 'S76-P-C2/VFR' AND deleted_at IS NULL;
 
 UPDATE modelos_sessao
 SET nome = 'SK76 - PERIÓDICO - 02/04 - CICLO 2: IFR',
     updated_at = datetime('now')
-WHERE codigo = 'SK76-C2-IFR' AND deleted_at IS NULL;
+WHERE codigo = 'S76-P-C2/IFR' AND deleted_at IS NULL;
 
 UPDATE modelos_sessao
 SET nome = 'SK76 - PERIÓDICO - 01/04 - CICLO 3: VFR',
     updated_at = datetime('now')
-WHERE codigo = 'SK76-C3-VFR' AND deleted_at IS NULL;
+WHERE codigo = 'S76-P-C3/VFR' AND deleted_at IS NULL;
 
 UPDATE modelos_sessao
 SET nome = 'SK76 - PERIÓDICO - 02/04 - CICLO 3: IFR',
     updated_at = datetime('now')
-WHERE codigo = 'SK76-C3-IFR' AND deleted_at IS NULL;
+WHERE codigo = 'S76-P-C3/IFR' AND deleted_at IS NULL;
