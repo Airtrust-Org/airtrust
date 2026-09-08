0deec7397474a6891cb9909767ca41951d9d97ed
isaeldaumas <isael.daumas@gmail.com>
2026-09-08 09:14:01 -0300

    test(rbac): update certificate permission mock

--- worker-airtrust/src/__tests__/routes/qualificacoes-certificados-gerar-idempotency.test.ts
+++ worker-airtrust/src/__tests__/routes/qualificacoes-certificados-gerar-idempotency.test.ts
@@ -35,6 +35,7 @@ vi.mock('../../middleware/tenant', async (importOriginal) => {
 });
 
 vi.mock('../../middleware/rbac', () => ({
+  requirePermission: () => async (_c: any, next: () => Promise<void>) => { await next(); },
   requireRole:
     (...roles: string[]) =>
     async (c: any, next: () => Promise<void>) => {
