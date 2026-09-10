d47764d27cdc5fa118b99fb2276674c58d7bb4ab
isaeldaumas <isael.daumas@gmail.com>
2026-09-09 22:12:34 -0300

    chore(pilot): rotate shell cache for lease trust update

--- public/pilot/pilot-sw.js
+++ public/pilot/pilot-sw.js
@@ -1,4 +1,4 @@
-const PILOT_CACHE_VERSION = 'airtrust-pilot-shell-v7';
+const PILOT_CACHE_VERSION = 'airtrust-pilot-shell-v8';
 const PILOT_SCOPE_PATH = '/pilot/';
 const PRECACHE_URLS = [
   '/pilot/',
