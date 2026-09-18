75ff22733673b0ae7a508bd95d558f42a31d6816
isaeldaumas <isael.daumas@gmail.com>
2026-09-18 12:49:46 -0300

    chore(pilot): refresh offline shell cache

--- public/pilot/pilot-sw.js
+++ public/pilot/pilot-sw.js
@@ -1,4 +1,4 @@
-const PILOT_CACHE_VERSION = 'airtrust-pilot-shell-v15';
+const PILOT_CACHE_VERSION = 'airtrust-pilot-shell-v16';
 const PILOT_SCOPE_PATH = '/pilot/';
 const PRECACHE_URLS = [
   '/pilot/',
