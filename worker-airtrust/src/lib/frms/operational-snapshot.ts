02b4b778057dbc917e568bc499e78b18569e259d
isaeldaumas <isael.daumas@gmail.com>
2026-09-25 15:19:47 -0300

    feat(frms): include activity rows

--- worker-airtrust/src/lib/frms/operational-snapshot.ts
+++ worker-airtrust/src/lib/frms/operational-snapshot.ts
@@ -142,6 +142,7 @@ export interface BuildOperationalSnapshotInput {
     jornadas: JornadaSnapshotRow[];
     checkins: CheckinSnapshotRow[];
     effectiveness: EffectivenessSnapshotRow[];
+    activities: FrmsActivitySnapshotRow[];
     funcionarios: FuncionarioSnapshotRow[];
   };
   filters?: FrmsOperationalSnapshotFilters;
